315a0eb9856d51431702d91fd439d3cb4316837d
HoshinoTented <limbolrain@gmail.com>
2022-08-24 02:57:56 +0800

    [ Refactor ] Refactor project structure for fun.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,12 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.10" apply false
 }
 
-group = "hoshino.otp"
-version = "1.0-SNAPSHOT"
+allprojects {
+    apply(plugin = "kotlin")
 
-repositories {
-    maven("https://maven.aliyun.com/repository/public")
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        mavenCentral()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,8 @@
 
 rootProject.name = "otp-kt"
 
+include(":opt-algo")
+
 pluginManagement {
     repositories {
         maven("https://maven.aliyun.com/repository/gradle-plugin")
